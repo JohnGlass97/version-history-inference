4d05f44f9b13bfd958aafc713fe5134f6dc915dd
JohnGlass97 <83819614+JohnGlass97@users.noreply.github.com>
2025-01-03 23:08:42 +0000

    Remove hello_world module

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 #![allow(unused)]
 
 mod diffing;
-mod hello_world;
-
-use crate::hello_world::get_hw_string;
 
 fn main() {
-    let hw = get_hw_string();
-    println!("{}", hw);
+    println!("Hello world!");
 }
