b13eb8cece6d6ed8b56e638859973c7192adc14d
JohnGlass97 <83819614+JohnGlass97@users.noreply.github.com>
2025-01-09 11:27:16 +0000

    Add basic arg parse setup

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #![allow(unused)]
 
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
+use clap::{arg, command, value_parser, Command};
 use engine::infer_version_tree;
 use render_as_tree::render;
 use rendering::produce_label_tree;
@@ -13,8 +14,27 @@ mod file_fetching;
 mod rendering;
 mod types;
 
+fn parse_args() -> PathBuf {
+    let matches = command!()
+        .subcommand(
+            Command::new("infer")
+                .about("Infer a version tree for the different versions represented by folders in the provided directory")
+                .arg(
+                    arg!(<dir> "Directory containing folders where each folder represents a version")
+                    .id("dir")
+                    .value_parser(value_parser!(PathBuf)),
+                )
+        )
+        .get_matches();
+
+    let submatches = matches.subcommand_matches("infer").unwrap();
+    submatches.get_one::<PathBuf>("dir").unwrap().to_path_buf()
+}
+
 fn main() {
-    let version_tree = infer_version_tree(Path::new("test_temp")).unwrap();
+    let dir = parse_args();
+
+    let version_tree = infer_version_tree(&dir).unwrap();
 
     let label_tree = produce_label_tree(&version_tree, None);
     print!("{}", render(&label_tree).join("\n"));
