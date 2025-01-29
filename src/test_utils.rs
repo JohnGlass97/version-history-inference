5485da4818c36e3337920757823ad2bd6fa29918
JohnGlass97 <83819614+JohnGlass97@users.noreply.github.com>
2025-01-29 16:22:50 +0000

    Add and use tempdir crate for testing

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -1,18 +1,8 @@
 use std::{
-    fs::{self, OpenOptions},
+    fs::OpenOptions,
     io::{self, Write},
     path::Path,
 };
-
-use crate::types::TreeNode;
-
-pub struct UseTestTempDir;
-
-impl Drop for UseTestTempDir {
-    fn drop(&mut self) {
-        fs::remove_dir_all("test_temp").unwrap();
-    }
-}
 
 pub fn append_to_file<P: AsRef<Path>, C: AsRef<[u8]>>(path: P, contents: C) -> io::Result<()> {
     let mut file = OpenOptions::new().append(true).open(path)?;
