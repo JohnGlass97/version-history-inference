9ee5f2691ced435ec846b727180d592ac81f6830
JohnGlass97 <83819614+JohnGlass97@users.noreply.github.com>
2025-03-26 21:45:35 +0000

    Add ConsoleTimer

--- src/bin/vhi.rs
+++ src/bin/vhi.rs
@@ -4,7 +4,11 @@ use clap::{arg, command, value_parser, Command};
 use render_as_tree::render;
 use std::path::{Path, PathBuf};
 
-use version_history_inference::{engine::infer_version_tree, rendering::produce_label_tree};
+use version_history_inference::{
+    engine::infer_version_tree,
+    rendering::produce_label_tree,
+    utils::{start_console_timer, stop_console_timer},
+};
 
 fn parse_args() -> PathBuf {
     let matches = command!()
@@ -26,7 +30,9 @@ fn parse_args() -> PathBuf {
 fn main() {
     let dir = parse_args();
 
+    let timer = start_console_timer();
     let version_tree = infer_version_tree(&dir).unwrap();
+    stop_console_timer(timer);
 
     let label_tree = produce_label_tree(&version_tree);
     print!("{}", render(&label_tree).join("\n"));
---
+++ src/utils.rs
@@ -0,0 +1,35 @@
+use std::{
+    sync::{atomic::AtomicBool, Arc},
+    thread::{self, JoinHandle},
+    time::{Duration, Instant},
+};
+
+pub struct ConsoleTimer(JoinHandle<()>, Arc<AtomicBool>);
+
+pub fn start_console_timer() -> ConsoleTimer {
+    let running = Arc::new(AtomicBool::new(true));
+    let running_clone = running.clone();
+
+    let handle = thread::spawn(move || {
+        let start = Instant::now();
+
+        while running_clone.load(std::sync::atomic::Ordering::Relaxed) {
+            let elapsed = start.elapsed();
+            print!(
+                "\nElapsed: {:02}:{:02}:{:02}",
+                elapsed.as_secs() / 3600,
+                (elapsed.as_secs() / 60) % 60,
+                elapsed.as_secs() % 60
+            );
+            std::io::Write::flush(&mut std::io::stdout()).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    ConsoleTimer(handle, running)
+}
+
+pub fn stop_console_timer(timer: ConsoleTimer) {
+    let ConsoleTimer(handle, running) = timer;
+    running.store(false, std::sync::atomic::Ordering::Relaxed);
+    handle.join().unwrap();
+}
