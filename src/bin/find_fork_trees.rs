cdfb046386f1daf97b58fda1b73911d7269b1e2e
JohnGlass97 <83819614+JohnGlass97@users.noreply.github.com>
2025-04-23 12:27:21 +0100

    Make find_fork_trees async

--- src/bin/find_fork_trees.rs
+++ src/bin/find_fork_trees.rs
@@ -7,6 +7,7 @@ use std::{
     io::Write,
 };
 
+use futures::future::join_all;
 use octocrab::Octocrab;
 use render_as_tree::render;
 use version_history_inference::{
@@ -15,7 +16,7 @@ use version_history_inference::{
 };
 
 async fn find_forks(octo: &Octocrab, owner: &str, repo: &str) -> TreeNode<VersionRef> {
-    let version_ref_tree = build_fork_tree(octo, owner, repo, 2, 2).await.unwrap();
+    let version_ref_tree = build_fork_tree(octo, owner, repo, 2, 3).await.unwrap();
 
     let label_tree = version_ref_tree.map(&|t| gen_version_display_name(t));
     print!("{}\n", render(&label_tree).join("\n"));
@@ -32,15 +33,18 @@ async fn main() {
     let test_repo_file = File::open("./test_repos.json").unwrap();
     let list: serde_json::Value = serde_json::from_reader(test_repo_file).unwrap();
 
-    let mut fork_trees: HashMap<String, TreeNode<VersionRef>> = HashMap::new();
+    let mut fork_tree_futures = vec![];
 
     for full_name in list.as_array().expect("Expected list of strings") {
         let full_name = full_name.as_str().expect("Expected list of strings");
         let (owner, repo) = split_full_name(full_name);
 
-        let tree = find_forks(&octo, owner, repo).await;
-        fork_trees.insert(format!("{repo}-forks"), tree);
+        let tree_future = find_forks(&octo, owner, repo);
+        fork_tree_futures.push(async move { (format!("{repo}-forks"), tree_future.await) });
     }
+
+    let fork_trees: HashMap<String, TreeNode<VersionRef>> =
+        join_all(fork_tree_futures).await.into_iter().collect();
 
     fs::create_dir_all("./test_repos");
     let mut file = File::create("./test_repos/fork_trees.json").unwrap();
