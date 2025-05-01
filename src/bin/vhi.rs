1a9a783e1bf00ce7bf2d5b0f1e59185f8b1828ff
JohnGlass97 <83819614+JohnGlass97@users.noreply.github.com>
2025-05-01 20:47:43 +0100

    Add view command

--- src/bin/vhi.rs
+++ src/bin/vhi.rs
@@ -4,7 +4,7 @@ use clap::{arg, command, value_parser, ArgAction, Command};
 use indicatif::{HumanDuration, MultiProgress, ProgressBar};
 use render_as_tree::render;
 use std::{
-    fs::File,
+    fs::{self, File},
     path::{Path, PathBuf},
     process::exit,
 };
@@ -21,6 +21,8 @@ use version_history_inference::{
 enum Config {
     /// directory, file extension, recursive, multithreading, trace_perf
     Infer(PathBuf, Option<String>, bool, bool, bool),
+    /// directory
+    View(PathBuf),
 }
 
 fn parse_args() -> Config {
@@ -49,17 +51,34 @@ fn parse_args() -> Config {
                     arg!(-p --"trace-performance" "Produce a JSON file with runtime duration information").action(ArgAction::SetTrue)
                 )
         )
+        .subcommand(
+            Command::new("view")
+                .about("View the previously produced version tree for the specified directory")
+                .arg(
+                    arg!(<dir> "Directory containing version_tree.json")
+                    .id("dir")
+                    .value_parser(value_parser!(PathBuf)),
+                )
+        )
         .get_matches();
 
-    let submatches = matches.subcommand_matches("infer").unwrap();
+    match matches.subcommand() {
+        Some(("infer", submatches)) => {
+            let dir = submatches.get_one::<PathBuf>("dir").unwrap().to_path_buf();
+            let ext = submatches.get_one::<String>("ext").cloned();
+            let recursive = submatches.get_flag("recursive");
+            let multithreading = !submatches.get_flag("no-multithreading");
+            let trace_perf = submatches.get_flag("trace-performance");
 
-    let dir = submatches.get_one::<PathBuf>("dir").unwrap().to_path_buf();
-    let ext = submatches.get_one::<String>("ext").cloned();
-    let recursive = submatches.get_flag("recursive");
-    let multithreading = !submatches.get_flag("no-multithreading");
-    let trace_perf = submatches.get_flag("trace-performance");
+            Config::Infer(dir, ext, recursive, multithreading, trace_perf)
+        }
+        Some(("view", submatches)) => {
+            let dir = submatches.get_one::<PathBuf>("dir").unwrap().to_path_buf();
 
-    Config::Infer(dir, ext, recursive, multithreading, trace_perf)
+            Config::View(dir)
+        }
+        _ => panic!("Command not recognised"), // This shouldn't happen with .subcommand_required(true)
+    }
 }
 
 fn save_version_tree(dir: &Path, diff_tree: &TreeNode<DiffInfo>) -> Result<(), String> {
@@ -100,7 +119,7 @@ fn infer(
 
     let diff_tree = produce_diff_tree(&version_tree);
     save_version_tree(&dir, &diff_tree).unwrap_or_else(|e| {
-        eprintln!("\nFailed to save version tree: {e}");
+        eprintln!("Failed to save version tree: {e}");
         exit(1);
     });
     perf_tracker.done_saving();
@@ -121,7 +140,28 @@ fn infer(
     }
 }
 
+fn view(dir: &Path) {
+    // Load tree
+    let version_tree_json = fs::read_to_string(dir.join("version_tree.json")).unwrap_or_else(|e| {
+        eprintln!("Couldn't load version_tree.json from the specified directory: {e}");
+        exit(1);
+    });
+    let version_tree: TreeNode<DiffInfo> =
+        serde_json::from_str(&version_tree_json).unwrap_or_else(|e| {
+            eprintln!("version_tree.json is malformed, maybe rerun the infer command: {e}");
+            exit(1);
+        });
+
+    // Output tree
+    let label_tree = produce_label_tree(&version_tree);
+    print!("{}", render(&label_tree).join("\n"));
+}
+
 fn main() {
-    let Config::Infer(dir, ext, recursive, multithreading, trace_perf) = parse_args();
-    infer(&dir, ext, recursive, multithreading, trace_perf);
+    match parse_args() {
+        Config::Infer(dir, ext, recursive, multithreading, trace_perf) => {
+            infer(&dir, ext, recursive, multithreading, trace_perf)
+        }
+        Config::View(dir) => view(&dir),
+    };
 }
