b2b606718b58dc38ce9fca4b813e3bd1189058f9
JohnGlass97 <83819614+JohnGlass97@users.noreply.github.com>
2025-05-12 20:30:42 +0100

    Add run_inference_all function

--- python_scripts/run_inference.py
+++ python_scripts/run_inference.py
@@ -4,41 +4,84 @@ from pathlib import Path
 
 DEFENDER_DISABLED = True
 MULTITHREADING = True
-START_AT_I = 0
 TAG_WITH_VERSION_COUNT = True
 
 TEST_REPOS = Path("./test_repos")
 
+RUNS = 5
 
-def run_inf():
-    for i, test_repo in enumerate(os.listdir(TEST_REPOS)):
-        test_dir = TEST_REPOS / test_repo
-        if not os.path.isdir(test_dir) or i < START_AT_I:
+
+def set_num_versions(versions_dir: Path, num_versions):
+    versions: list[str] = os.listdir(versions_dir)
+
+    included_count = 0
+    for version in versions:
+        curr = versions_dir / version
+
+        if not os.path.isdir(curr):
+            os.rename(curr, versions_dir / version.removeprefix("ignore_"))
             continue
 
-        no_versions = sum(os.path.isdir(test_dir / d)
-                          for d in os.listdir(test_dir))
+        include = included_count < num_versions
+        needs_changed = include == version.startswith("ignore_")
 
-        if no_versions < 2:
+        if needs_changed:
+            if include:
+                os.rename(curr, versions_dir / version.removeprefix("ignore_"))
+            else:
+                os.rename(curr, versions_dir / f"ignore_{version}")
+
+        if include:
+            included_count += 1
+
+    assert included_count <= num_versions
+
+    return included_count == num_versions
+
+
+def run_inference_on(test_repos_dir: Path, test_repo: str, version_counts):
+    for num_versions in version_counts:
+        curr_dir = test_repos_dir / test_repo
+
+        # Check if enough versions
+        versions_set = set_num_versions(curr_dir, num_versions)
+        if not versions_set:
+            # This check should un-`ingore_` all versions
+            break
+
+        for run in range(1, RUNS + 1):
+            print(f"NOW STARTING {test_repo} ({num_versions} versions) run {run}")
+
+            # Bypass cache
+            new_dir = test_repos_dir / f"{test_repo}_{run}"
+            os.rename(curr_dir, new_dir)
+            curr_dir = new_dir
+
+            defender = "_no_defender" if DEFENDER_DISABLED else ""
+            mt = "" if MULTITHREADING else "_no_mt"
+            v_tag = f"_{num_versions}_versions" if TAG_WITH_VERSION_COUNT else ""
+
+            trace_name = f"perf_trace{mt}{defender}{v_tag}_{run}.json"
+
+            mt_flag = "" if MULTITHREADING else "--no-multithreading"
+            command = f".\\target\\release\\vhi.exe infer -d {mt_flag} -p {trace_name} {curr_dir}"
+            subprocess.run(command, shell=True, check=True)
+
+        os.rename(curr_dir, test_repos_dir / test_repo)
+
+
+def run_inference_all():
+    for test_repo in os.listdir(TEST_REPOS):
+        if not os.path.isdir(TEST_REPOS / test_repo):
             continue
 
-        print("NOW STARTING", i, test_repo, f"({no_versions} versions)")
-
-        defender = "_no_defender" if DEFENDER_DISABLED else ""
-        mt = "" if MULTITHREADING else "_no_mt"
-        tag = f"_{no_versions}_versions" if TAG_WITH_VERSION_COUNT else ""
-
-        trace_name = f"perf_trace{mt}{defender}{tag}.json"
-
-        mt_flag = "" if MULTITHREADING else "--no-multithreading"
-        command = f".\\target\\release\\vhi.exe infer -d {mt_flag} -p {trace_name} {test_dir}"
-        subprocess.run(command, shell=True, check=True)
-
-    print("DONE")
+        run_inference_on(TEST_REPOS, test_repo, list(range(1, RUNS + 1)))
 
 
 if __name__ == "__main__":
-    while DEFENDER_DISABLED and input("Is Defender disabled? y/n: ") != "y":
+    while DEFENDER_DISABLED and input("Is Defender disabled? y/n: ") == "n":
         pass
+
     os.chdir("..")
-    run_inf()
+    print("Current working directory:", os.getcwd())
+    run_inference_all()
