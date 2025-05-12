484ab40ee9de94bcf0027b45ab3b52a1088fff70
JohnGlass97 <83819614+JohnGlass97@users.noreply.github.com>
2025-05-12 16:47:34 +0100

    Support averaging for time_vs_versions

--- python_scripts/time_vs_versions.py
+++ python_scripts/time_vs_versions.py
@@ -7,7 +7,7 @@ TEST_REPOS = Path("./test_repos")
 STAGES = ["load_versions", "infer"]
 
 
-def get_time_vs_versions() -> pd.DataFrame:
+def get_time_vs_versions(trace_prefix: str = "perf_trace_no_defender_") -> pd.DataFrame:
     results_df = pd.DataFrame(
         columns=["name", "stage"] + [str(i) for i in range(2, 21, 2)])
 
@@ -16,22 +16,34 @@ def get_time_vs_versions() -> pd.DataFrame:
         if not os.path.isdir(test_dir):
             continue
 
-        new_rows = {s: {"name": test_repo, "stage": s} for s in STAGES}
+        times = {k: {} for k in STAGES}
 
         for file in os.listdir(test_dir):
             if os.path.isdir(test_dir / file):
                 continue
 
-            if not file.startswith("perf_trace_no_defender_") or not file.endswith("_versions.json"):
+            if not file.startswith(trace_prefix) or not file.endswith("_versions.json"):
                 continue
 
             data = json.load(open(test_dir / file))
 
             for stage in STAGES:
-                new_rows[stage][data["no_versions"]] = data[f"{stage}_rt"]
+                v_no = data["no_versions"]
+                if v_no not in times[stage]:
+                    times[stage][v_no] = []
+
+                times[stage][v_no].append(float(data[f"{stage}_rt"]))
+
+        new_rows = []
+        for stage, t_dict in times.items():
+            row = {v_no: str(sum(t_list) / len(t_list))
+                   for v_no, t_list in t_dict.items()}
+            row["name"] = test_repo
+            row["stage"] = stage
+            new_rows.append(row)
 
         results_df = pd.concat(
-            [results_df, pd.DataFrame(new_rows.values())], ignore_index=True)
+            [results_df, pd.DataFrame(new_rows)], ignore_index=True)
 
     return results_df
 
